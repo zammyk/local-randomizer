e9e74ed564eece8a992869b1364ffb770c1f0103
Rahul Hembrom <rhembrom21@gmail.com>
2023-12-02 18:55:35 +0530

    Add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+  <Navbar></Navbar>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld'
+import Navbar from './components/Navbar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Navbar
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,body {
+  padding: 0;
+  margin: 0;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+<nav class="navbar">
+    <div class="container">
+        <div class="title">Randomizer</div>
+        <div style="margin-left: auto; display: flex; gap: 1rem; align-items: center;">
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+        </div>
+    </div>
+</nav>
+</template>
+
+<style scoped>
+.navbar {
+background-color: #333;
+color: #fff;
+padding: 10px 0;
+}
+
+.container {
+display: flex;
+align-items:end;
+padding: 0 1rem;
+align-items: center;
+}
+
+a {
+color: #fff;
+text-decoration: none;
+}
+
+.title {
+    font-size: 2rem;
+}
+</style>
