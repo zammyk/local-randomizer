e30db00534a36b22f0d73454dd51fd9a243b2295
Rahul Hembrom <rhembrom21@gmail.com>
2023-12-07 23:54:39 +0530

    Indentation fix

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -103,60 +103,60 @@ gap: 5rem;
 }
 
 nav div {
-    --c: #444;
-    color: var(--c);
-    font-size: 16px;
-    border: 0.3em solid var(--c);
-    border-radius: 0.5em;
-    width: 12em;
-    height: 3em;
-    text-transform: uppercase;
-    font-weight: bold;
-    font-family: sans-serif;
-    letter-spacing: 0.1em;
-    text-align: center;
-    line-height: 3em;
-    position: relative;
-    overflow: hidden;
-    z-index: 1;
-    transition: 0.5s;
-    margin: 1em;
+--c: #444;
+color: var(--c);
+font-size: 16px;
+border: 0.3em solid var(--c);
+border-radius: 0.5em;
+width: 12em;
+height: 3em;
+text-transform: uppercase;
+font-weight: bold;
+font-family: sans-serif;
+letter-spacing: 0.1em;
+text-align: center;
+line-height: 3em;
+position: relative;
+overflow: hidden;
+z-index: 1;
+transition: 0.5s;
+margin: 1em;
 }
 
 nav div span {
-    position: absolute;
-    width: 25%;
-    height: 100%;
-    background-color: var(--c);
-    transform: translateY(150%);
-    border-radius: 50%;
-    left: calc((var(--n) - 1) * 25%);
-    transition: 0.5s;
-    transition-delay: calc((var(--n) - 1) * 0.1s);
-    z-index: -1;
+position: absolute;
+width: 25%;
+height: 100%;
+background-color: var(--c);
+transform: translateY(150%);
+border-radius: 50%;
+left: calc((var(--n) - 1) * 25%);
+transition: 0.5s;
+transition-delay: calc((var(--n) - 1) * 0.1s);
+z-index: -1;
 }
 
 nav div:hover {
-    color: black;
+color: black;
 }
 
 nav div:hover span {
-    transform: translateY(0) scale(2);
+transform: translateY(0) scale(2);
 }
 
 nav div span:nth-child(1) {
-    --n: 1;
+--n: 1;
 }
 
 nav div span:nth-child(2) {
-    --n: 2;
+--n: 2;
 }
 
 nav div span:nth-child(3) {
-    --n: 3;
+--n: 3;
 }
 
 nav div span:nth-child(4) {
-    --n: 4;
+--n: 4;
 }
 </style>
